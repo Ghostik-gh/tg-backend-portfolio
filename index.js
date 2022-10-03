323a3c85c3f98c58109a9a1fd3f24d57e2d6bad6
Ghostik-gh <feodor200@mail.ru>
2022-10-04 00:40:32 +0300

    try web portfolio on tg

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TG_API_KEY;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const urlPortfolio = 'https://ghostik-gh.github.io/portfolio/#/';
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'Received your message', {
+      reply_markup: {
+        inline_keyboard: [[{ text: 'Сайт', web_app: { url: urlPortfolio } }]],
+      },
+    });
+  }
+});
