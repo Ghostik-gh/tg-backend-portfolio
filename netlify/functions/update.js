cce1e090df447f68e07c379570a6ec81d0a1ff95
Ghostik-gh <feodor200@mail.ru>
2022-10-04 12:31:59 +0300

    Respond to messages

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,4 +1,15 @@
+require('dotenv').config();
+const axios = require('axios').default;
 exports.handler = async (event) => {
   console.log('Received an update from Telegram!', event.body);
   return { statusCode: 200 };
 };
+
+await axios.post(
+  `https://api.telegram.org/bot${process.env.TG_API_KEY}/sendMessage`,
+  {
+    // TODO: Add form data
+    chat_id: JSON.parse(event.body).message.chat.id,
+    text: 'I got your message!',
+  }
+);
