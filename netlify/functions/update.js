587584caf42026a51a11fe2429c18d354d3e6778
Ghostik-gh <feodor200@mail.ru>
2022-10-04 12:44:50 +0300

    async

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 const axios = require('axios').default;
 exports.handler = async (event) => {
   console.log('Received an update from Telegram!', event.body);
+  await axios.post(
+    `https://api.telegram.org/bot${process.env.TG_API_KEY}/sendMessage`,
+    {
+      // TODO: Add form data
+      chat_id: JSON.parse(event.body).message.chat.id,
+      text: 'I got your message!',
+    }
+  );
   return { statusCode: 200 };
 };
-
-await axios.post(
-  `https://api.telegram.org/bot${process.env.TG_API_KEY}/sendMessage`,
-  {
-    // TODO: Add form data
-    chat_id: JSON.parse(event.body).message.chat.id,
-    text: 'I got your message!',
-  }
-);
